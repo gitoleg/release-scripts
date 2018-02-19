256620a92be24a217a086f29789113291ef63810
Oleg <forown@yandex.ru>
2018-02-19 13:25:54 -0500

    minor update of release script

--- src/release.sh
+++ src/release.sh
@@ -14,13 +14,14 @@ source check.sh
 
 url=$1
 version=$2
+base=master
 
 check_version $version
 setup_packages $3 $4
 
 prerelease_package() {
     pkg=$1
-    oldpath=packages/$pkg/$pkg.master
+    oldpath=packages/$pkg/$pkg.$base
     newpath=packages/$pkg/$pkg.$version
     rm -rf $newpath
     cp -r $oldpath $newpath
@@ -31,6 +32,6 @@ for package in $packages; do
     if [ "$needs_update" == "true" ]; then
         prerelease_package $package
         ./set-url.sh $url $version $package
-        ./change-version.sh "master" $version $package
+        ./change-version.sh $base $version $package
     fi
 done
